cf0d7ff3f714f64ce355386daa6e6ee4522ebcc2
MSI\Minh Tan <tanlun04112000@gmail.com>
2021-11-19 14:16:42 +0700

    Login Google

--- app/src/main/java/com/example/subprojectma14ctttantritoanlenhuyn5/MainActivity.java
+++ app/src/main/java/com/example/subprojectma14ctttantritoanlenhuyn5/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.subprojectma14ctttantritoanlenhuyn5;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
@@ -9,14 +11,68 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private int RC_SIGN_IN = 1;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(MainActivity.this);
+        if (acct != null) {
+
+        }
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if(result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+            startActivity(new Intent(this, Screen_Home.class));
+            finish();
+        }else {
+
+        }
     }
 
     public void showHidePass(View view){
--- app/src/main/java/com/example/subprojectma14ctttantritoanlenhuyn5/Screen_Home.java
+++ app/src/main/java/com/example/subprojectma14ctttantritoanlenhuyn5/Screen_Home.java
@@ -1,14 +1,27 @@
 package com.example.subprojectma14ctttantritoanlenhuyn5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.LinkedList;
 
-public class Screen_Home extends AppCompatActivity {
+public class Screen_Home extends AppCompatActivity implements View.OnClickListener{
+
+    private int RC_SIGN_IN = 1;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,17 @@ public class Screen_Home extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_home);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(Screen_Home.this);
+        if (acct != null) {
+            findViewById(R.id.button2).setVisibility(View.VISIBLE);
+        }
 
         rv_food = findViewById(R.id.rv);
         rv_food_1 = findViewById(R.id.rv_1);
@@ -34,6 +58,26 @@ public class Screen_Home extends AppCompatActivity {
         rv_food_1.setAdapter(adapterHomeFood);
         rv_food_1.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
+        findViewById(R.id.button2).setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button2:
+                signOut();
+                break;
+        }
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        startActivity(new Intent(Screen_Home.this, MainActivity.class));
+                        finish();
+                    }
+                });
     }
 }
